65b70790da51a78d7367e88cae36e67ec3d9f6b7
John Jeffers <john@jeffers.cc>
2026-01-01 16:19:06 -0700

    v1.1.4

--- casks/luxury-yacht.rb
+++ casks/luxury-yacht.rb
@@ -3,12 +3,12 @@ cask "luxury-yacht" do
   desc "Luxury Yacht is a desktop app for managing Kubernetes clusters"
 
   app "Luxury Yacht.app"
-  version "v1.1.3"
+  version "v1.1.4"
 
-  # arch arm: "arm64", intel: "amd64"
-  arch arm: "arm64"
-  sha256 arm: "a8b168b9aa12f4fc923f5a727a1314ee17f483e3cf2fb083a6312ad0387f1de6"
-         # intel: "REPLACE_WITH_AMD64_SHA256"
+  arch   arm: "arm64"
+         intel: "amd64"
+  sha256 arm: "4b209d6f5eda78cb7cfe94a0dbe8793c15c18422fce84c4be984248c1091a079"
+         intel: "08e8ca30f1e8f6d8a6dbc740f7d258a7a59579eba51be2fff35fa4762d3f3dfe"
 
   url "https://github.com/luxury-yacht/app/releases/download/#{version}/luxury-yacht-#{version}-macos-#{arch}.dmg",
     verified: "github.com/luxury-yacht/app/"
